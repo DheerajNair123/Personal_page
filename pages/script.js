5d6d47cebc5a5732eb9d364eb22559cbe51f232e
DheerajNair123 <dheerajnairp@gmail.com>
2024-10-19 19:35:40 +0530

    Added all files from Aug to Sep

---
+++ pages/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('loading').style.display = 'none';
+    }, 3000); // Change 3000 to your desired timeout duration
+
+};
